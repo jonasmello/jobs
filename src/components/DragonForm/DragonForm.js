6f5b5e6b860604b3f061c08f31dbd1630b636f06
Jonas Mello <jonas.mello@dbccompany.com.br>
2020-05-18 13:53:42 -0300

    Added dragon form validation.

--- src/components/DragonForm/DragonForm.js
+++ src/components/DragonForm/DragonForm.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 
 import { FaDragon } from 'react-icons/fa';
 import Header from '../Header';
+import FormField from '../FormField/FormField';
 
 function DragonForm({ saveDragon, dragonData = {}, text = {} }) {
   const [name, setName] = useState(dragonData.name || '');
   const [type, setType] = useState(dragonData.type || '');
+  const [formFeedback, setFormFeedback] = useState(text.description);
   const { id } = dragonData;
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (name === '' || type === '') {
+      setFormFeedback('Por favor, preencha todos os campos');
+      return false;
+    }
     const data = { name, type, id };
     saveDragon(data);
   };
@@ -22,13 +28,13 @@ function DragonForm({ saveDragon, dragonData = {}, text = {} }) {
             <FaDragon />
           </div>
           <h1 className="h1">{text.title}</h1>
-          <p className="body-text">{text.description}:</p>
+          <p className="body-text">{formFeedback}:</p>
           <form action="" method="post" onSubmit={handleSubmit}>
             <div>
-              <input placeholder="Nome" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+              <FormField placeholder="Nome" type="text" name="name" value={name} setState={setName} />
             </div>
             <div>
-              <input placeholder="Tipo" type="text" name="type" value={type} onChange={(e) => setType(e.target.value)} />
+              <FormField placeholder="Tipo" type="text" name="type" value={type} setState={setType} />
             </div>
             <div>
               <button className="form-button" type="submit">
